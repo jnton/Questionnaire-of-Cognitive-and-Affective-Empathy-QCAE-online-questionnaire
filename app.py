bab99e0a836e2a9e981b0785b70422fff3fef6a2
JnTon <84038748+JnTon@users.noreply.github.com>
2023-08-21 03:07:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ st.set_page_config(
     page_title="Questionnaire of Cognitive and Affective Empathy (QCAE)",
 )
 
+st.subheader("Questionnaire of Cognitive and Affective Empathy")
+st.markdown('<p style="font-size: 10px;">Renate L. E. P. Reniers, Rhiannon Corcoran, Richard Drake, Nick M. Shryane & Birgit A. Völlm (2011). The QCAE: A Questionnaire of Cognitive and Affective Empathy. <i>Journal of Personality Assessment.</i> <a href="https://doi.org/10.1080/00223891.2010.528484">https://doi.org/10.1080/00223891.2010.528484</a></p>', unsafe_allow_html=True)
+
 # Define the questionnaire items and response options
 QCAE_items = [
     "I sometimes find it difficult to see things from the “other guy’s” point of view.",
